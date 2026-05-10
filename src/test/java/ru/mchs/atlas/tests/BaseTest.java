039a867994144e64a4938f91dbc4d3b967931728
K <103377916+ktokar06@users.noreply.github.com>
2026-05-10 03:43:34 +0300

    Update BaseTest.java

--- src/test/java/ru/mchs/atlas/tests/BaseTest.java
+++ src/test/java/ru/mchs/atlas/tests/BaseTest.java
@@ -11,7 +11,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Parameters;
-import ru.mchs.atlas.utils.NetworkBlocker;
 import ru.mchs.atlas.listeners.TestListener;
 
 import java.net.MalformedURLException;
